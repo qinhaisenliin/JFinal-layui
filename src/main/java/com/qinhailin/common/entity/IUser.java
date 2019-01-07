e6cecc8b1f908a432fe10c60b029b3bfd8845850
琴海森林 <qinhaisenlin@163.com>
2019-01-07 23:10:02 +0800

    更新 IUser.java

--- src/main/java/com/qinhailin/common/entity/IUser.java
+++ src/main/java/com/qinhailin/common/entity/IUser.java
@@ -1,57 +1,34 @@
 package com.qinhailin.common.entity;
 
+import java.io.Serializable;
+
 /**
- *
  * 用户实体类接口
- *
- * @author hhs
- * @date 2017年11月16日
  */
-public interface IUser {
+public interface IUser extends Serializable{
 
 	/**
-	 *
 	 * 获取用户ID
-	 *
-	 * @return
-	 * @author hhs
-	 * @date 2017年11月16日
 	 */
 	public String getId();
 
 	/**
 	 * 获取用户编号
-	 *
-	 * @return
-	 * @author hhs
-	 * @date 2017年11月16日
 	 */
 	public String getUserCode();
 
 	/**
 	 * 获取用户名
-	 * 
-	 * @return
-	 * @author hhs
-	 * @date 2017年11月16日
 	 */
 	public String getUserName();
 
 	/**
 	 * 获取机构ID
-	 * 
-	 * @return
-	 * @author hhs
-	 * @date 2017年11月16日
 	 */
 	public String getOrgId();
 
 	/**
 	 * 获取密码
-	 * 
-	 * @return
-	 * @author hhs
-	 * @date 2017年11月16日
 	 */
 	public String getPassword();
 
