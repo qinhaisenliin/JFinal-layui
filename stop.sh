f31cdc7efc55afa1223b3ae1a067357acf6d33f0
琴海森林 <qinhaisenlin@163.com>
2019-01-09 19:03:20 +0800

    更新 stop.sh 更正启动类地址，避免给攻城狮带啦麻烦

--- stop.sh
+++ stop.sh
@@ -21,7 +21,7 @@
 # ---------------------------------------------------------------------------
 
 # 启动入口类，该脚本文件用于别的项目时要改这里
-MAIN_CLASS=com.jfinal.club.common.JFinalClubConfig
+MAIN_CLASS=com.qinhailin.common.config.MainConfig
 
 # kill 命令不使用 -9 参数时，项目被正常、温和地关闭，jfinal 项目中的回调会起作用
 kill `pgrep -f ${MAIN_CLASS}` 2>/dev/null
