ea1bea9ec50da0d544059e2a8c292a04ad724c35
QinHaiSenLin <qinhaisenlin@163.com>
2020-06-06 04:00:10 +0800

    完善报表功能

--- src/main/java/com/qinhailin/common/ureport/Ureport2BuildinDatasource.java
+++ src/main/java/com/qinhailin/common/ureport/Ureport2BuildinDatasource.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019-2020 覃海林(qinhaisenlin@163.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 package com.qinhailin.common.ureport;
 
 import java.sql.Connection;
@@ -7,11 +22,15 @@ import com.bstek.ureport.definition.datasource.BuildinDatasource;
 import com.jfinal.plugin.druid.DruidPlugin;
 import com.qinhailin.common.config.MainConfig;
 
+/**
+ * ureport2数据源
+ * @author QinHaiLin
+ * @date 2020-06-06
+ */
 public class Ureport2BuildinDatasource implements BuildinDatasource {
 
 	@Override
 	public String name() {
-		
 		return "mainDataSource";
 	}
 
